4fb1d29a005e5034a96efe5d1742ec42f7c4a714
Akhilesh Thite <akhileshthite@hotmail.com>
2023-02-14 01:38:45 +0530

    update greeter.test.js

--- test/greeter.test.js
+++ test/greeter.test.js
@@ -1,22 +1,21 @@
-// greeter.test.js
-const Greeter = artifacts.require("Greeter");
 const { expect } = require("chai");
+const hre = require("hardhat");
 
-contract("Greeter", (accounts) => {
+describe("Greeter", function () {
   let greeter;
 
   beforeEach(async () => {
-    greeter = await Greeter.new();
+    greeter = await hre.ethers.getContractFactory("Greeter");
   });
 
   it("Should return the initial message", async () => {
     const result = await greeter.read();
-    expect(result).to.equal("Hello, world!");
+    expect(result).equal("Hello, world!");
   });
 
   it("Should be able to change the message", async () => {
     await greeter.write("Goodbye, world!");
     const result = await greeter.read();
-    expect(result).to.equal("Goodbye, world!");
+    expect(result).equal("Goodbye, world!");
   });
 });
